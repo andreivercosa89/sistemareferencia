6f108a416f15912acab0c2698df2310d3c0fc8a0
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-05 20:11:38 -0400

    Criando um objeto

---
+++ sistemareferencia/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace sistemareferencia
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+
+        public void Dados(){
+            Console.WriteLine("Nome: {0}\nIdade:{1}\nEndereco: {2}", Nome, Idade, Endereco);
+        }
+
+        public Pessoa (string Enderco){
+            this.Endereco = Enderco;
+        }
+
+
+        public Pessoa()
+        {
+
+        }
+    }
+}
---
+++ sistemareferencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sistemareferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa("Rua 7 de Setembro");
+
+            Console.WriteLine("Informe o Nome: ");
+            pessoa.Nome = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("\nInforme a Idade: ");
+            pessoa.Idade = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInforme o Endereco: ");
+            pessoa.Endereco = Console.ReadLine();
+
+            Console.Clear();
+            pessoa.Dados();
+        }
+    }
+}
